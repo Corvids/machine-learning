16dc65b5475f9d4ab06862de656bed7746e7d522
Corvids <joonnifer@gmail.com>
2017-01-11 16:46:06 -0600

    add logs for learning

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -63,7 +63,7 @@ class LearningAgent(Agent):
         ###########
         # Set 'state' as a tuple of relevant data for the agent
         agent_inputs = tuple([inputs[item] for item in inputs])
-        state = (waypoint,agent_inputs)
+        state = (waypoint, agent_inputs, deadline)
 
         return state
 
